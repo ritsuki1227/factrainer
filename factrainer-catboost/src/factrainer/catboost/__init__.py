3e7a531ba514b9183074abb61affa72f50c1f14e
ritsuki1227 <ritsuki1227@gmail.com>
2025-04-17 19:39:06 +0900

    not impl warning

---
+++ factrainer-catboost/src/factrainer/catboost/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "The 'factrainer.catboost' module is currently a placeholder and not yet implemented. "
+    "Functionality will be added in a future release.",
+    UserWarning,
+    stacklevel=2,
+)
---
+++ factrainer-sklearn/src/factrainer/sklearn/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "The 'factrainer.sklearn' module is currently a placeholder and not yet implemented. "
+    "Functionality will be added in a future release.",
+    UserWarning,
+    stacklevel=2,
+)
